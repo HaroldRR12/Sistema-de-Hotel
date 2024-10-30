ab158a4c351765502aab94f8c7a5d1ae76104a65
jeremy99351 <Leinersm00@gmail.com>
2024-10-29 21:59:56 -0600

    leiner
    creacion de clase de reserva y de modo de guardar en un txt

---
+++ SistemaDeHotel/src/sistema/de/hotel/clases/reservacion.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistema.de.hotel.clases;
+
+public class reservacion {
+    private String id,usuario,idAbitacion,nombre;
+    private int precio;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getIdAbitacion() {
+        return idAbitacion;
+    }
+
+    public void setIdAbitacion(String idAbitacion) {
+        this.idAbitacion = idAbitacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+    
+}
